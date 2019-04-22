3d04f5f8c7c66a37fa9a6684f33f8ef73ee57fe1
Tomasi <Eric.Tomasi@sunovion.com>
2019-04-22 13:28:46 -0400

    feat: turned code into a function for generate_df

--- data/data.py
+++ data/data.py
@@ -4,33 +4,34 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-#SQL Alchemy connection
-engine = create_engine('oracle://{}:{}@{}'.format(os.environ.get('USER'), os.environ.get('PW'), os.environ.get('DB')))
-
-conn = engine.connect()
-
-# Construct a sessionmaker object
-Session = sessionmaker()
-
-# Bind the sessionmaker to engine
-Session.configure(bind=engine)
-
-#Session
-db_session = Session()
-
-
-
-
 def sql_query(filename):
+    '''reads in a .sql file and returns string so it can be read into pandas.'''
+
     # Open and read the file as a single buffer
     with open(filename, 'r') as fd:
         sqlFile = fd.read()
         return sqlFile
 
 
+def generate_df():
+    '''returns base pandas dataframe from query.sql'''
+    #SQL Alchemy connection
+    engine = create_engine('oracle://{}:{}@{}'.format(os.environ.get('USER'), os.environ.get('PW'), os.environ.get('DB')))
 
-#df = pd.read_sql(sql_query('query.sql'), con=conn)
+    conn = engine.connect()
 
+    # Construct a sessionmaker object
+    Session = sessionmaker()
 
-db_session.close()
-conn.close()
+    # Bind the sessionmaker to engine
+    Session.configure(bind=engine)
+
+    #Session
+    db_session = Session()
+
+    df = pd.read_sql(sql_query('query.sql'), con=conn)
+
+    db_session.close()
+    conn.close()
+
+    return df
