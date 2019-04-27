e1e62b9b7706d33ea2d17ec3b40752760275d2d3
Tomasi <Eric.Tomasi@sunovion.com>
2019-04-27 16:32:33 -0400

    feat: imlpemented filter_df function

--- src/model.py
+++ src/model.py
@@ -1,5 +1,20 @@
 from data.data import generate_df
 
-df = generate_df('data/roi.sql')
 
-print(df.head())
+def filter_df(roi):
+    '''Inputs the roi dataframe and applies various filters and sorts it and returns the new dataframe'''
+
+    roi['wk'] = roi['wk'].astype('int')
+
+    roi = roi.sort_values('wk', ascending=True).reset_index()
+
+    del roi['index']
+
+    roi['t'] = roi.index + 1
+
+    return roi
+
+roi = generate_df('data/roi.sql')
+roi = filter_df(roi)
+
+print(roi.head())
