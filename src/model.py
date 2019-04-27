abd9a5042f4d28f85af84745ee6d872e3dcc897c
Tomasi <Eric.Tomasi@sunovion.com>
2019-04-27 17:48:48 -0400

    feat: implemented regression function and updated pipfile

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ from data.data import generate_df
 
 
 def filter_df(roi):
-    '''Inputs the roi dataframe and applies various filters and sorts it and returns the new dataframe'''
+    '''Inputs a datafram and applies various filters and sorts input dataframe and returns the new dataframe'''
 
     roi['wk'] = roi['wk'].astype('int')
 
@@ -14,7 +14,50 @@ def filter_df(roi):
 
     return roi
 
-roi = generate_df('data/roi.sql')
-roi = filter_df(roi)
 
-print(roi.head())
+def regression(df):
+    '''Inputs a dataframe, runs a linear regression and returns a new datafram with predictions'''
+
+    import statsmodels.formula.api as sm
+    from scipy import stats
+    import pandas as pd
+    import numpy as np
+
+    df = filter_df(df)
+
+    #regression
+    regression = sm.ols(formula="vol ~ t", data=df).fit()
+
+    #prediction dataframe
+    predict = pd.DataFrame({'t': range(1,53)})
+
+    #setting varibales based on regression output
+    intercept = regression.params.Intercept
+    slope = regression.params.t
+    se_regression = np.sqrt(regression.scale)
+    observations = regression.nobs
+    avg = np.mean(predict['t'])
+    stdev = np.std(predict['t'])
+    df_residual = regression.df_resid
+    t_crit = stats.t.ppf(1-.025, df_residual)
+
+    #creating prediction column in dataframe
+    predict['prediction'] = (intercept + predict['t']*slope)
+
+    #Creating columns for upper and lower bound of the prediciton based on 95% confidence interval
+    predict['SE_of_prediction'] = se_regression*(np.sqrt(1+(1/observations)+((predict['t']-avg)**2)/((stdev**2)*(observations-1))))
+
+    predict['margin_of_error'] = predict['SE_of_prediction'] * t_crit
+
+    predict['Lower Bound'] = predict['prediction'] - predict['margin_of_error']
+    predict['Upper Bound'] = predict['prediction'] + predict['margin_of_error']
+
+    #show regression results in terminal
+    print(regression.summary())
+
+    return predict
+
+df = generate_df('data/roi.sql')
+df = regression(df)
+
+print(df.head())
