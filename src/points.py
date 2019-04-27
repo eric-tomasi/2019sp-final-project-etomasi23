83db4fdc89dde4fed848b01970b8ae831fe0968b
Tomasi <Eric.Tomasi@sunovion.com>
2019-04-27 13:43:51 -0400

    feat: implemented competitors_growing function

--- src/points.py
+++ src/points.py
@@ -3,4 +3,12 @@ from data.data import generate_df
 
 df = generate_df('data/query.sql')
 
-print(df.head())
+def competitors_growing(df):
+    '''Assigns 4 points to record if competitor volume is growing'''
+
+    if ( (df['comp1_curr_vol'] > df['comp2_prev_vol']) or (df['comp2_curr_vol'] > df['comp2_prev_vol']) ):
+        return 4
+    else:
+        return 0
+
+#print(df.head())
