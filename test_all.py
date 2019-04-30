99055f4f0c8530ae15c91873969d44033ee0f53d
Tomasi <Eric.Tomasi@sunovion.com>
2019-04-30 12:26:41 -0400

    feat: implemented test_sun_250 function

--- test_all.py
+++ test_all.py
@@ -37,3 +37,20 @@ def test_flat_decline_sun():
     assert df.iloc[19,-1] == -5
     assert df.iloc[17,-1] == -7
     assert df.iloc[1,-1] == 0
+
+
+def test_high_comp():
+
+    df['test'] = df.apply(high_comp, axis=1)
+
+    assert df.iloc[0,-1] == 0
+    assert df.iloc[2,-1] == 2
+    assert df.iloc[15,-1] == 2
+
+
+def test_sun_250():
+
+    df['test'] = df.apply(sun_250, axis=1)
+
+    assert df.iloc[0,-1] == 0.5
+    assert df.iloc[26,-1] == 0
