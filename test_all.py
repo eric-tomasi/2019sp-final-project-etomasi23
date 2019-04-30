f2ac386972b14e25a39eb9fec0b7a81be2feed45
Tomasi <Eric.Tomasi@sunovion.com>
2019-04-30 12:19:20 -0400

    feat: implemented test_flat_decline_sun function

--- test_all.py
+++ test_all.py
@@ -26,3 +26,14 @@ def test_competitors_growing():
 
     assert df.iloc[0,-1] == 4
     assert df.iloc[3,-1] == 0
+
+
+
+def test_flat_decline_sun():
+
+    df['test'] = df.apply(flat_decline_sun, axis=1)
+
+    assert df.iloc[23,-1] == 5
+    assert df.iloc[19,-1] == -5
+    assert df.iloc[17,-1] == -7
+    assert df.iloc[1,-1] == 0
